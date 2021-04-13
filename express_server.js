aadbc2b991db006b380f738714d88f3e9426cde0
vagrant <suki.ckchan@gmail.com>
2021-04-13 05:56:15 +0000

    express_server.js

--- express_server.js
+++ express_server.js
@@ -2,19 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 
-app.set("view engin", "ejs");
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.set("view engine", "ejs");
+
 app.get("/", (req,res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls/:shortURL", (req,res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
 });
 
 app.get ("/hello", (req, res) => {
