62c2bbc27c618d3b35b845e074296610defda4d1
vagrant <suki.ckchan@gmail.com>
2021-04-14 08:30:43 +0000

    Cookies

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const morgan = require('morgan');
 
 const generateRandomString = function() {
@@ -15,6 +16,7 @@ const urlDatabase = {
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -31,16 +33,26 @@ app.get("/", (req,res) => {
 // });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req,res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { 
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
@@ -66,6 +78,16 @@ app.post("/urls/:id", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL
   delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
+app.post("/login", (req, res) => {
+  res.cookie("username", username);
+  res.redirect('/urls');
+});
+
+app.post("logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
